0531bda2d73a0a6c81e14c625d02ff282533592f
erik dexter <erikdexter21@gmail.com>
2023-12-28 06:06:22 +0700

    features: plugin treesitter for highlighting, indenting, folding, and many other things (especially for text editing)

---
+++ lua/plugins/plugin/nvim-treesitter.lua
@@ -0,0 +1,71 @@
+local config = function()
+  require("nvim-treesitter.configs").setup({
+    -- basic things
+    build = ":TSUpdate",
+
+    indent = {
+      enable = true,
+    },
+
+    autotag = {
+      enable = true,
+    },
+
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+
+    ensure_installed = {
+      "markdown",
+      "jsonc",
+      "javascript",
+      "typescript",
+      "yaml",
+      "html",
+      "css",
+      "markdown",
+      "bash",
+      "lua",
+      "dockerfile",
+      "gitignore",
+      "python",
+      "python",
+    },
+
+    auto_install = true,
+
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-s>",
+        node_incremental = "<C-s>",
+        scope_incremental = false,
+        node_decremental = "<BS>",
+      },
+    },
+
+    rainbow = {
+      enable = true,
+      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      max_file_lines = nil, -- Do not enable for files with more than n lines, int
+      -- colors = {}, -- table of hex strings
+      -- termcolors = {} -- table of colour name strings
+    },
+  })
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = config,
+}
