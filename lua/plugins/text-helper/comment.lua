f61a952780329b18a075f149ceff1570490ffc9b
erik dexter <erikdexter21@gmail.com>
2023-12-28 06:24:09 +0700

    features: plugin text helper for more efficiency while writting some code regardless of language

---
+++ lua/plugins/text-helper/comment.lua
@@ -0,0 +1,14 @@
+return {
+  "numToStr/Comment.nvim",
+  lazy = false,
+  opts = {
+    toggler = {
+      line = "<C-_>",
+      block = "<C-_>",
+    },
+    opleader = {
+      line = "<C-_>",
+      block = "<C-_>",
+    },
+  },
+}
---
+++ lua/plugins/text-helper/nvim-ts-autotag.lua
@@ -0,0 +1,13 @@
+local config = function()
+  require'nvim-ts-autotag'
+end
+
+return {
+	"windwp/nvim-ts-autotag",
+	lazy = false,
+  opts = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = config
+}
