2f410d3e46eebeb1d92cdde51b012ee5ef042354
erik dexter <erikdexter21@gmail.com>
2023-12-28 06:16:16 +0700

    features: plugin auto session for undo/redo last session in neovim

---
+++ lua/plugins/plugin/auto-session.lua
@@ -0,0 +1,13 @@
+local config = function()
+  local auto_session = require("auto-session")
+
+  auto_session.setup({
+    auto_restore_enabled = false,
+    auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+  })
+end
+
+return {
+  "rmagatti/auto-session",
+  config = config,
+}
