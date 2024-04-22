24af6dd573d14964df42c1ebe66741da9b84ac6d
Erikdexter <erikdexter21@gmail.com>
2024-04-23 06:55:35 +0700

    modifie: moved some key bindings from keymap.lua to whichkey.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,27 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+-- Selecting
+keymap.set("n", "<C-a>", "gg<S-v>G", opts) -- Select all
+vim.api.nvim_set_keymap("n", "gg<S-v>G", "<Nop>", opts)
+keymap.set("n", "ds", "vb_d", opts) -- Delete a word backward
+vim.api.nvim_set_keymap("n", "vb_d", "<Nop>", opts)
+
+-- Redo
+keymap.set("n", "r", "<C-r>", opts) -- Redo
+vim.api.nvim_set_keymap("n", "<C-r>", "<Nop>", opts)
+
+-- Noice History
+keymap.set("n", "<leader>n", "<cmd>NoiceTelescope<CR>", { desc = "Noice History" })
+
+-- Noice History
+-- keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+-- keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 
 -- Normal
 -- keymap.set({ "i", "v" }, "jk", "<ESC>", opts)      -- Normal mode
@@ -13,24 +34,13 @@ local opts = { noremap = true, silent = true }
 -- vim.api.nvim_set_keymap('n', 'A', '<Nop>', opts)
 -- vim.api.nvim_set_keymap('x', '[[:normal! I]]', '<Nop>', opts)
 
--- Selecting
-keymap.set("n", "<C-a>", "gg<S-v>G", opts) -- Select all
 -- keymap.set("n", "<C-f>", "viw", opts)      -- Select a word
 -- keymap.set("n", "<C-y>", "yaw", opts)      -- Select and copy a word
 -- keymap.set("n", "<C-d>", "daw", opts)      -- Select and cut a word
 -- keymap.set("n", "<C-c>", "viwc", opts)     -- Select and edit a word
-vim.api.nvim_set_keymap('n', 'gg<S-v>G', '<Nop>', opts)
-
--- Noice History
-keymap.set("n", "<leader>n", "<cmd>NoiceTelescope<CR>", { desc = "Noice History" })
-
--- Interactive search and replace
-keymap.set('n', '<leader>Sr', [[:%s/\<C-R><C-W>\>//gc<Left><Left><Left>]], { desc = "Write the word" })
 
 -- keymap.set("n", "dr", "dw", opts) -- Delete a single word
 -- keymap.set("n", "df", "D", opts)  -- Delete a word forward
-keymap.set("n", "ds", "vb_d", opts)  -- Delete a word backward
-vim.api.nvim_set_keymap('n', 'vb_d', '<Nop>', opts)
 -- vim.api.nvim_set_keymap('n', 'dw', '<Nop>', opts)
 -- vim.api.nvim_set_keymap('n', 'D', '<Nop>', opts)
 
@@ -40,90 +50,32 @@ vim.api.nvim_set_keymap('n', 'vb_d', '<Nop>', opts)
 -- vim.api.nvim_set_keymap('n', '>>', '<Nop>', opts)
 -- vim.api.nvim_set_keymap('n', '<<', '<Nop>', opts)
 
--- Redo
-keymap.set("n", "r", "<C-r>", opts) -- Redo
-vim.api.nvim_set_keymap('n', '<C-r>', '<Nop>', opts)
-
-
--- Set keybinds for Vim-maximizer
-keymap.set("n", "<leader>sm", "<cmd>MaximizerToggle<CR>", {desc = "Maximize/minimize a split"})
-
-
--- Set keybinds for Tagbar
-keymap.set("n", "<leader>Tt", "<cmd>TagbarToggle<CR>", {desc = "Open Tagbar"})
-keymap.set("n", "<leader>Tc", "<cmd>TagbarClose<CR>", {desc = "Close Tagbar"})
-
-
--- Set keybinds for ToggleTerm
--- keymap.set("n", "<leader>th", "<Cmd>ToggleTerm size=10 direction=horizontal<Cr>", { desc = "Horizontal Terminal" })
--- keymap.set("n", "<leader>tv", "<Cmd>ToggleTerm size=45 direction=vertical<Cr>", { desc = "Vertical Terminal" })
--- keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float size=45<cr>", { desc = "Floating Terminal" })
-
-
--- Set keybinds for Live-Sever
-keymap.set("n", "<leader>Ls", "<cmd>LiveServerStart<CR>", { desc = "Start Live-Sever" })
-keymap.set("n", "<leader>Lc", "<cmd>LiveServerStop<CR>", { desc = "Stop Live-Sever" })
-
-
--- Set keybinds for Telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find your keymaps" })
-
-
--- Set keybinds for LSP
-keymap.set("n", "<leader>gL", "<cmd>Telescope lsp_references<CR>", { desc = "Show LSP references" })
-keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Show LSP definitions" })
-keymap.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" })
-keymap.set("n", "<leader>gy", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions" })
-keymap.set({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, { desc = "See available code actions" })
-keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { desc = "Smart rename" })
-keymap.set("n", "<leader>gb", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Show buffer diagnostics" })
-keymap.set("n", "<leader>gl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
-keymap.set("n", "<leader>gm", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-keymap.set("n", "<leader>gn", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-keymap.set("n", "<leader>gK", vim.lsp.buf.hover, { desc = "Show documentation for what is under cursor" })
-keymap.set("n", "<leader>gR", ":LspRestart<CR>", { desc = "Restart LSP" })
-
-
-
 local mapkey = require("util.keymapper").mapkey
 
 -- Show Full File-Path
-mapkey("<leader>p", "echo expand('%:p')", "n", { desc = "Show Current Path" }) -- Show Full File Path
-
--- Buffer Navigation
-mapkey("<leader>bn", "bnext", "n", { desc = "Next buffer" })                          -- Next buffer
-mapkey("<leader>bp", "bprevious", "n", { desc = "Previous buffer" })                  -- Prev buffer
-mapkey("<leader>bb", "e #", "n", { desc = "Switch buffer" })                          -- Switch to Other Buffer
-mapkey("<leader>bd", "bdelete", "n", { desc = "Delete buffer" })                      -- Deleting current buffer
-mapkey("<leader>bq", "BufferLinePickClose", "n", { desc = "Pick and delete buffer" }) -- Close buffer by picking
+-- mapkey("<leader>p", "echo expand('%:p')", "n", { desc = "Show Current Path" }) -- Show Full File Path
 
 -- Directory Navigation
-mapkey("<leader>j", ":Neotree<CR>", "n", { desc = "Open Side Bar" })
-mapkey("<leader>k", ":Neotree close<CR>", "n", { desc = "close Side Bar" })
+-- mapkey("<leader>j", ":Neotree<CR>", "n", { desc = "Open Side Bar" })
+mapkey("<C-c>", ":Neotree close<CR>", "n", { desc = "close Side Bar" })
 mapkey("<C-b>", ":Neotree buffers<CR>", "n", { desc = "Buffers" })
 mapkey("<C-g>", ":Neotree git_status<CR>", "n", { desc = "Git Status" })
 mapkey("<C-f>", ":Neotree filesystem<CR>", "n", { desc = "File System" })
 
 -- Navigation
-mapkey("<C-h>", "wincmd h", "t")          -- Navigate Left
-mapkey("<C-j>", "wincmd j", "t")          -- Navigate Down
-mapkey("<C-k>", "wincmd k", "t")          -- Navigate Up
-mapkey("<C-l>", "wincmd l", "t")          -- Navigate Right
-mapkey("<C-h>", "TmuxNavigateLeft", "n")  -- Navigate Left
-mapkey("<C-j>", "TmuxNavigateDown", "n")  -- Navigate Down
-mapkey("<C-k>", "TmuxNavigateUp", "n")    -- Navigate Up
+mapkey("<C-h>", "wincmd h", "t") -- Navigate Left
+mapkey("<C-j>", "wincmd j", "t") -- Navigate Down
+mapkey("<C-k>", "wincmd k", "t") -- Navigate Up
+mapkey("<C-l>", "wincmd l", "t") -- Navigate Right
+mapkey("<C-h>", "TmuxNavigateLeft", "n") -- Navigate Left
+mapkey("<C-j>", "TmuxNavigateDown", "n") -- Navigate Down
+mapkey("<C-k>", "TmuxNavigateUp", "n") -- Navigate Up
 mapkey("<C-l>", "TmuxNavigateRight", "n") -- Navigate Right
 
 -- Window Management
-mapkey("<leader>sv", "vsplit", "n", { desc = "Split Vertically" })        -- Split Vertically
-mapkey("<leader>sh", "split", "n", { desc = "Split Horizontally" })       -- Split Horizontally
-mapkey("<leader>se", "<C-w>=", "n", { desc = "Split window equal" })      -- make split windows equal width & height
-mapkey("<leader>sx", "close<CR>", "n", { desc = "Close current window" }) -- close current split window
+mapkey("<leader>sM", "<c-w>T", "n", { desc = "Maximize Window" }) -- Split Vertically
+mapkey("<leader>sm", "<c-w>^", "n", { desc = "Minimize Window" }) -- Split Horizontally
+mapkey("<leader>se", "<C-w>=", "n", { desc = "Split window equal" }) -- make split windows equal width & height
 -- * Window Navigation
 mapkey("<C-h>", "<C-w>h", "n")
 mapkey("<C-j>", "<C-w>j", "n")
@@ -135,12 +87,7 @@ mapkey("<C-Down>", "resize -2", "n")
 mapkey("<C-Left>", "vertical resize +2", "n")
 mapkey("<C-Right>", "vertical resize -2", "n")
 
-
-local api = vim.api
-
--- Comments
-api.nvim_set_keymap("n", "<leader>c", "gcc", { desc = "Line Comment" })
--- api.nvim_set_keymap("v", "<leader>cb", "gbc", { desc = "Block comment" })
+-- local api = vim.api
 
 -- Debugger
 -- api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<Cr>", { noremap = true })
--- lua/plugins/standard/_whichkey.lua
+++ lua/plugins/standard/_whichkey.lua
@@ -1,87 +1,112 @@
 local config = function()
-  local wk = require("which-key")
-  wk.register({
-      --[[ c = {
-        name = "Placeholder",
-        r = { "Placeholder" }
-      }, ]]
-      f = {
-        name = " Telescope",
-        c = { "Find String Under Cursor" },
-        f = { "Find File" },
-        k = { "Find Keymaps" },
-        r = { "Find Recent File" },
-        s = { "Find String" },
-      },
-      S = {
-        name = " Search & Replace",
-        r = { "Write Down The Word" }
-      },
-      g = {
-        name = " LSP",
-        a = { "See Available Code Actions" },
-        b = { "Show Buffer Diagnostics" },
-        D = { "Go to declaration" },
-        d = { "Show LSP Definitions" },
-        i = { "Show LSP Implementations" },
-        K = { "Show Documentation For What Is Under Cursor" },
-        L = { "Show LSP References" },
-        l = { "Show Line Diagnostics" },
-        m = { "Go To Previous Diagnostic" },
-        n = { "Go To Next Diagnostic" },
-        R = { "Restart LSP" },
-        r = { "Smart Rename" },
-        y = { "Show LSP Type Definitions" },
-      },
-      b = {
-        name = " Buffer Settings",
-        b = { "Switch Buffer" },
-        d = { "Delete Buffer" },
-        n = { "Next Buffer" },
-        p = { "Previous Buffer" },
-        q = { "Pick And Delete Buffer" },
-      },
-      s = {
-        name = " Windows Management",
-        e = { "Split Window Equal" },
-        h = { "Split Window Horizontal" },
-        m = { "Maximize/Minimize A Window" },
-        v = { "Split Window Vertically" },
-        x = { "Close Current Window" },
-      },
-      L = {
-        name = " Live Server",
-        s = { "Start Live Server" },
-        c = { "Stop Live Server" },
-      },
-      t = {
-        name = " Terminal",
-        f = { "Floating Terminal" },
-        h = { "Horizontal Terminal" },
-        v = { "Vertical Terminal" },
-      }
-    },
-    { prefix = "<leader>" })
+	local wk = require("which-key")
+	wk.register({
+		['"'] = {
+			name = " Trouble",
+			w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Trouble Workspace Diagnostic" },
+			d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Trouble Document Diagnostic" },
+			c = { "<cmd>TroubleClose<CR>", "Close Trouble Diagnostic" },
+		},
+		["/"] = {
+			name = " Terminal",
+			f = { "<cmd>ToggleTerm direction=float size=45<cr>", "Floating Terminal" },
+			h = { "<Cmd>ToggleTerm size=10 direction=horizontal<Cr>", "Horizontal Terminal" },
+			v = { "<Cmd>ToggleTerm size=45 direction=vertical<Cr>", "Vertical Terminal" },
+		},
+		["<leader>"] = {
+			name = " Telescope",
+			c = { "<cmd>Telescope grep_string<cr>", "Find String Under Cursor" },
+			f = { "<cmd>Telescope find_files<cr>", "Find File" },
+			k = { "<cmd>Telescope keymaps<cr>", "Find Keymaps" },
+			r = { "<cmd>Telescope oldfiles<cr>", "Find Recent File" },
+			s = { "<cmd>Telescope live_grep<cr>", "Find String" },
+		},
+		b = {
+			name = " Buffer Settings",
+			b = { "<cmd>e #<cr>", "Switch Buffer" },
+			d = { "<cmd>bdelete<cr>", "Delete Buffer" },
+			n = { "<cmd>bnext<cr>", "Next Buffer" },
+			m = { "<cmd>bprevious<cr>", "Previous Buffer" },
+			p = { "<cmd>BufferLinePick<cr>", "Pick Buffer" },
+			q = { "<cmd>BufferLinePickClose<cr>", "Pick And Delete Buffer" },
+		},
+		-- G = {
+		-- 	name = " Glow",
+		-- 	e = { "<cmd>Glow<CR>", "Open Markdown Preview" },
+		-- 	d = { "<cmd>Glow!<CR>", "Close Markdown Preview" },
+		-- },
+		g = {
+			name = " LSP",
+			a = { vim.lsp.buf.code_action, "See Available Code Actions" },
+			b = { "<cmd>Telescope diagnostics bufnr=0<CR>", "Show Buffer Diagnostics" },
+			D = { vim.lsp.buf.declaration, "Go to declaration" },
+			d = { "<cmd>Telescope lsp_definitions<CR>", "Show LSP Definitions" },
+			i = { "<cmd>Telescope lsp_implementations<CR>", "Show LSP Implementations" },
+			K = { vim.lsp.buf.hover, "Show Documentation Under Cursor" },
+			L = { "<cmd>Telescope lsp_references<CR>", "Show LSP References" },
+			l = { vim.diagnostic.open_float, "Show Line Diagnostics" },
+			m = { vim.diagnostic.goto_prev, "Go To Previous Diagnostic" },
+			n = { vim.diagnostic.goto_next, "Go To Next Diagnostic" },
+			R = { ":LspRestart<CR>", "Restart LSP" },
+			r = { vim.lsp.buf.rename, "Smart Rename" },
+			y = { "<cmd>Telescope lsp_type_definitions<CR>", "Show LSP Type Definitions" },
+		},
+		-- L = {
+		-- 	name = " Live Server",
+		-- 	s = { "<cmd>LiveServerStart<CR>", "Start Live Server" },
+		-- 	c = { "<cmd>LiveServerStop<CR>", "Stop Live Server" },
+		-- },
+		o = {
+			name = " Outline",
+			c = { "<cmd>OutlineClose<CR>", "Close Outline" },
+			o = { "<cmd>OutlineOpen<CR>", "Open Outline Side Bar" },
+			f = { "<cmd>OutlineFollow<CR>", "Go To Corresponding Node" },
+			s = { "<cmd>OutlineStatus<CR>", "Outline Status" },
+		},
+		S = {
+			name = " Search & Replace",
+			r = { [[:%s/\<C-R><C-W>\>//gc<Left><Left><Left>]], "Write Down The Word" },
+		},
+		s = {
+			name = " Windows Management",
+			e = { "", "Split Window Equal" },
+			h = { "Split", "Split Window Horizontal" },
+			m = { "", "Maximize/Minimize A Window" },
+			v = { "vsplit", "Split Window Vertically" },
+			x = { "close<cr>", "Close Current Window" },
+		},
+		T = {
+			name = " Twilight",
+			e = { "<cmd>TwilightEnable<CR>", "Enable Twilight" },
+			d = { "<cmd>TwilightDisable<CR>", "Disable Twilight" },
+		},
+		t = {
+			name = " Todo Comment",
+			l = { "<cmd>TodoLocList<CR>", "Todo Lock List" },
+			q = { "<cmd>TodoQuickFix<CR>", "Todo Quick Fix" },
+			r = { "<cmd>TodoTrouble<CR>", "Todo Trouble" },
+			t = { "<cmd>TodoTelescope<CR>", "Todo Telescope" },
+		},
+	}, { prefix = "<leader>" })
 end
 
 local init = function()
-  vim.o.timeout = true
-  vim.o.timeoutlen = 300
+	vim.o.timeout = true
+	vim.o.timeoutlen = 300
 end
 
-
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = init,
-  opts = {
-    mode = "n",
-    prefix = "",
-    buffer = nil,
-    silent = true,
-    noremap = true,
-    nowait = false,
-    expr = false,
-  },
-  config = config,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = init,
+	opts = {
+		mode = "n",
+		prefix = "",
+		buffer = nil,
+		silent = true,
+		noremap = true,
+		nowait = false,
+		expr = false,
+	},
+	config = config,
 }
