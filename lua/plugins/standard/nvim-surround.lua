216282eb48509f3b0265ff502813e5fcd4da09ee
Erikdexter <erikdexter21@gmail.com>
2024-04-23 07:04:37 +0700

    remove: deteling nvim surround (plugin)

--- lua/plugins/standard/nvim-surround.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "kylechui/nvim-surround",
-  event = { "BufReadPre", "BufNewFile" },
-  version = "*", -- Use for stability; comit to use `main` branch for the latest features
-  config = true,
-}
